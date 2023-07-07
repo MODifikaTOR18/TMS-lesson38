803aefb1833827d6b9aab079dcd89a62de480728
Andrey Yakovlev <modifikator97@gmail.com>
2023-07-07 16:59:49 +0300

    fix 10

--- libs/build/vars/stageImageCreate.groovy
+++ libs/build/vars/stageImageCreate.groovy
@@ -1,17 +1,9 @@
-def stageBuildCreate(app, path, lock_id) {
+def stageImageCreate(app, path, lock_id) {
     return {
         stage(app) {
-            lock("Build-lock-${lock_id}") {
-                dir(path) {
-                    sh """
-                        docker rm -f ${app}
-                        [ -d target] || mkdir target
-                        docker build -t ${app} -f Dockerfile-build .
-                        docker run --name ${app} ${app} mvn test &&
-                        docker cp ${app}:/app/target/ target/
-                        docker rm -f ${app}
-                    """
-                    builtApps.put(app, path)
+            lock("Image-create-lock-${lock_id}") {
+                dir (path) {
+                    sh "docker build -t ${DOCKER_REGISTRY}/${app}:${TAG} -f Dockerfile-create ."
                 }
             }
         }
