406b26e4172bc249b027ef05bf00a2813e431065
Andrey Yakovlev <modifikator97@gmail.com>
2023-07-13 09:43:02 +0300

    Fixies

--- libs/deploy/vars/stageDeploy.groovy
+++ libs/deploy/vars/stageDeploy.groovy
@@ -1,7 +1,7 @@
 def call(apps) {
     stageList = []
     stageMap = [:]
-    apps.each { app ->
+    apps.each { app, path ->
         stageMap.put(app, stageRun(app, env.TAG))
     }
     stageList.add(stageMap)
